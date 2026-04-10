b910ddcd3e1c8b57d224d7e81f84d6e6280055c9
veronika <sustv153@gmail.com>
2026-04-10 22:15:24 +0300

    api

--- lib/api/notes.ts
+++ lib/api/notes.ts
@@ -2,12 +2,22 @@ import axios from "axios";
 
 const BASE_URL = "https://notehub-public.goit.study/api";
 
-export async function fetchNotes() {
-  const res = await axios.get(`${BASE_URL}/notes`, {
-    headers: {
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
-    },
-  });
+const TOKEN = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
 
+const instance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: `Bearer ${TOKEN}`,
+  },
+});
+
+
+export async function fetchNotes() {
+  const res = await instance.get("/notes");
+  return res.data;
+}
+
+export async function fetchNoteById(id: string) {
+  const res = await instance.get(`/notes/${id}`);
   return res.data;
 }
