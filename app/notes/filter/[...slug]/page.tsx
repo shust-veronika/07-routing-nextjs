29c39ed7551a8260c6f1d668fb6cc9cd1beaa08a
veronika <sustv153@gmail.com>
2026-04-11 18:48:58 +0300

    hf

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -25,7 +25,6 @@ export default async function NotesPage({ params }: Props) {
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      {}
       <NoteList tag={tag} />
     </HydrationBoundary>
   );
--- components/NoteList/NoteList.tsx
+++ components/NoteList/NoteList.tsx
@@ -1,11 +1,22 @@
+'use client';
+
+import { useQuery } from "@tanstack/react-query";
+import { fetchNotes } from "@/lib/api";
 import Link from "next/link";
 import { Note } from "@/types/note";
 
 type Props = {
-  notes: Note[];
+  tag?: string;
 };
 
-export default function NoteList({ notes }: Props) {
+export default function NoteList({ tag }: Props) {
+  const { data } = useQuery({
+    queryKey: ["notes", { tag }],
+    queryFn: () => fetchNotes(tag),
+  });
+
+  const notes: Note[] = data?.notes || [];
+
   return (
     <ul>
       {notes.map((note) => (
