4fa2082db4e6c74082b76aced2770343bac6e236
veronika <sustv153@gmail.com>
2026-04-11 19:10:31 +0300

    jj

--- components/NoteList/NoteList.tsx
+++ components/NoteList/NoteList.tsx
@@ -1,21 +1,17 @@
 'use client';
 
-import { useQuery } from "@tanstack/react-query";
-import { fetchNotes } from "@/lib/api";
 import Link from "next/link";
 import { Note } from "@/types/note";
 
 type Props = {
   tag?: string;
+  notes?: Note[];
 };
 
-export default function NoteList({ tag }: Props) {
-  const { data } = useQuery({
-    queryKey: ["notes", { tag }],
-    queryFn: () => fetchNotes(tag),
-  });
-
-  const notes: Note[] = data?.notes || [];
+export default function NoteList({ notes }: Props) {
+  if (!notes || notes.length === 0) {
+    return <p>Нотаток не знайдено.</p>;
+  }
 
   return (
     <ul>
